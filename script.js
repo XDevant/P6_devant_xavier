73bdda68a74ee3d453f21b8e1e721fa23ff57653
XDevant <xdevant@gmail.com>
2022-03-09 22:15:52 +0100

    scroll ok, moda ok, need divs and breakpoints

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
 // Modal
-let modal = document.querySelector(".modal");
-let images = document.querySelectorAll("img");
-let span = document.querySelector(".close");
-
-
 // When the user clicks on a movie picture, open the modal
-for (let i=0; i<images.length; i++){
-    images[i].onclick = (event) => {
-        modal.style.display = "block";
-        load_data(event);
-      }
-}
+// Wait until the DOM is loaded
+window.addEventListener("DOMContentLoaded", function(){
+    let images = document.querySelectorAll("img");
+    let modal = document.querySelector(".modal");
+    let button = document.querySelector(".close");
 
-// Fills modal list
-let load_data = async (event) => {
-    data = await fetchData(event.target.name)
-    modal.querySelector("h3").innerText = data.title;
-    modal.querySelector("img").src = data.image_url;
-    let rows = modal.querySelectorAll("li");
-    rows[0].innerText = `Genre : ${data.genres[0]}`;
-    rows[1].innerText = `Date de sortie : ${data.date_published}`;
-    rows[2].innerText = `Evaluation : ${data.rated}`;
-    rows[3].innerText = `Score IMDB : ${data.imdb_score}`;
-    rows[4].innerText = `Réalisateur : ${data.directors[0]}`;
-    rows[5].innerText = `Acteurs : ${data.actors[0]}`;
-    rows[6].innerText = `Durée : ${data.duration}mn`;
-    rows[7].innerText = `Pays d'origine : ${data.countries[0]}`;
-    rows[8].innerText = `Box-office : ${data.directors}`;
-    rows[9].innerText = `Résumé : ${data.long_description}`;
-}
+    let modalKey = (event) => {
+     // Check for Tab key
+        if (event.keyCode !== 9 & modal.style.display !== "block") {
+            modal.style.display = "block";
+            load_data(event);
+        } else {
+            modal.style.display = "none";
+        }
+    }
+  
+    let modalClick = (event) => {
+        if (modal.style.display !== "block") {
+            modal.style.display = "block";
+            load_data(event);
+        } else {
+            modal.style.display = "none";
+        }
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = () => {
-  modal.style.display = "none";
-}
+    let load_data = async (event) => {
+        data = await fetchData(event.target.name)
+        modal.querySelector("h3").innerText = data.title;
+        modal.querySelector("img").src = data.image_url;
+        let rows = modal.querySelectorAll("li");
+        rows[0].innerText = `Genre : ${data.genres.join(', ')}`;
+        rows[1].innerText = `Date de sortie : ${data.date_published}`;
+        rows[2].innerText = `Evaluation : ${data.rated}`;
+        rows[3].innerText = `Score IMDB : ${data.imdb_score}`;
+        rows[4].innerText = `Réalisateur : ${data.directors.join(', ')}`;
+        rows[5].innerText = `Acteurs : ${data.actors.join(', ')}`;
+        rows[6].innerText = `Durée : ${data.duration}mn`;
+        rows[7].innerText = `Pays d'origine : ${data.countries.join(', ')}`;
+        rows[8].innerText = `Résultats Box-office : ${data.worldwide_gross_income ? data.worldwide_gross_income + ' ' + data.budget_currency : "Inconnu"}`;
+        rows[9].innerText = `Résumé : ${data.long_description}`;
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-modal.onclick = (event) => {
-    modal.style.display = "none";
-}
+    for (let i=0; i<images.length; i++){
+        images[i].addEventListener("click", modalClick);
+        images[i].addEventListener("keydown", modalKey);
+    }
+    button.addEventListener("click", () => modal.style.display = "none");
+    modal.addEventListener("click", () => modal.style.display = "none");
+});
+
+
 
 // Fetch n-bests movies
 let baseUrl = "http://localhost:8000/api/v1/titles";
@@ -96,51 +108,42 @@ loadCategory("sci-fi")
 // navigate galeries
 let leftButtons = document.querySelectorAll(".left button");
 let rightButtons = document.querySelectorAll(".right button");
-let counters = {
-    "best": 0,
-    "animation": 0,
-    "fantasy": 0,
-    "sci-fi": 0
+
+let foldImage = event => {
+    let category = event.target.className;
+    let width = document.querySelector(`.galery.${category} img`).clientWidth;
+    let scrolled = document.querySelector(`.galery.${category}`).scrollLeft;
+    let trailing = scrolled % width;
+    document.querySelector(`.galery.${category}`).scrollBy({
+        top: 0,
+        left: width - trailing,
+        behavior: 'smooth'
+      });
 }
 
-let foldImage = category => {
-    let counter = counters[category];
-    if (counter < 3) {
-        document.querySelectorAll(`.galery.${category} img`)[counter].style.width = 0;
-        document.querySelectorAll(`.galery.${category} img`)[counter].style.minWidth = 0;
-        document.querySelectorAll(`.galery.${category} img`)[counter].style.padding = 0;
-        counters[category] += 1;
+let unfoldImage = event => {
+    let category = event.target.className;
+    let width = document.querySelector(`.galery.${category} img`).clientWidth;
+    let scrolled = document.querySelector(`.galery.${category}`).scrollLeft;
+    let trailing = scrolled % width;
+    if (trailing != 0) {
+        width = trailing;
     }
+    document.querySelector(`.galery.${category}`).scrollBy({
+        top: 0,
+        left: -width,
+        behavior: 'smooth'
+      });
 }
 
-let unfoldImage = category => {
-    let counter = counters[category];
-    if (counter > 0) {
-        document.querySelectorAll(`.galery.${category} img`)[counter - 1].style.width = "auto";
-        document.querySelectorAll(`.galery.${category} img`)[counter - 1].style.minWidth = "25%";
-        document.querySelectorAll(`.galery.${category} img`)[counter - 1].style.padding = "1vw 2vw";
-        counters[category] -= 1
-    }
+let switchButtons = (catégory) => {
+
 }
 
 for (let i=0; i<leftButtons.length; i++) {
-    leftButtons[i].onclick = (event) => {
-        let category = event.target.name;
-        unfoldImage(category);
-      }
-    rightButtons[i].onclick = (event) => {
-        let category = event.target.name;
-        foldImage(category);
-      }
+    let category = leftButtons[i].className;
+    leftButtons[i].addEventListener("click", unfoldImage);
+    leftButtons[i].addEventListener("keyup", unfoldImage);
+    rightButtons[i].addEventListener("click", foldImage);
+    rightButtons[i].addEventListener("keyup", foldImage);
 }
-
-
-
-
-
-/* let myBlob = await response.blob();
-let objectURL = URL.createObjectURL(myBlob);
-let image = document.createElement('img');
-document.body.appendChild(image);
-    } catch(e) {
-*/
